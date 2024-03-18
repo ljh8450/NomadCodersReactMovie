05d257d1d135be20e3fc5b203bb8c16299f1373d
Miniberry <dzigh30@gmail.com>
2024-03-19 00:26:25 +0900

    How many can i buy coins?!

--- react-for-beginners/react-for-beginners/src/App.js
+++ react-for-beginners/react-for-beginners/src/App.js
@@ -3,30 +3,41 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [value, setValue] = useState(0);
+  const [cost, setCost] = useState(0);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
         setLoading(false);
+        setCost(67072.6635261777);
       });
   }, []);
+
+  const inputHandler = (event) => setValue(event.target.value);
+  const setChange = (event) => setCost(event.target.value);
   return(
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} 
+        <select onChange={setChange}>
+          {coins.map((coin, index) => (
+            <option key={index} value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
             </option>
           ))}
         </select>
       )}
+      <br/>
+      <p>I have <input dir="rtl" onChange={inputHandler}/>$</p>
+      <h2>You can exchange {value == 0 ? "0": Math.floor(value/cost)} Coins!</h2>
+
     </div>
   );
+  
 }
 
 export default App;
